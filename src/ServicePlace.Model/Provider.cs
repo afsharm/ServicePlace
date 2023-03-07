f45b38de07929de952f872a5b87bfa4a197df2e1
afshar <afshar.mohebbi@gmail.com>
2023-03-07 12:47:07 +0330

    using annotations

--- src/ServicePlace.Model/Provider.cs
+++ src/ServicePlace.Model/Provider.cs
@@ -1,7 +1,10 @@
-﻿namespace ServicePlace.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServicePlace.Model;
 public class Provider
 {
     public int Id { get; set; }
 
+    [MaxLength(100)]
     public string? Name { get; set; }
 }
