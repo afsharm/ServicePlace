982cf116ad8de79529ac15603b052f6d7bb8d301
afshar <afshar.mohebbi@gmail.com>
2023-03-07 12:07:26 +0330

    Added sqlite

--- src/ServicePlace.Web/Program.cs
+++ src/ServicePlace.Web/Program.cs
@@ -1,3 +1,5 @@
+using ServicePlace.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSqlite<ServicePlaceContext>("Data Source=ServicePlace.db");
 
 var app = builder.Build();
 
