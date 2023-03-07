d4173f1492e07bd490ce5abf84244f4dbbda14d7
afshar <afshar.mohebbi@gmail.com>
2023-03-07 13:52:20 +0330

    added services

---
+++ src/ServicePlace.Service/AppService.cs
@@ -0,0 +1,21 @@
+﻿using ServicePlace.Data;
+using ServicePlace.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace ServicePlace.Service;
+public class AppService
+{
+    private readonly ServicePlaceContext _context;
+
+    public AppService(ServicePlaceContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<Provider> GetAll()
+    {
+        return _context.Providers
+            .AsNoTracking()
+            .ToList();
+    }
+}
