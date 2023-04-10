71b7cdf64755e8c929e2db2d8c915e9d60494dad
afshar <afshar.mohebbi@gmail.com>
2023-04-10 11:48:06 +0330

    command and query in the service

--- src/ServicePlace.Service/AppService.cs
+++ src/ServicePlace.Service/AppService.cs
@@ -1,6 +1,8 @@
 ﻿using ServicePlace.Data;
-using ServicePlace.Model;
+using ServicePlace.Model.Entities;
 using Microsoft.EntityFrameworkCore;
+using ServicePlace.Model.Queries;
+using ServicePlace.Model.Commands;
 
 namespace ServicePlace.Service;
 public class AppService
@@ -17,5 +19,24 @@ public class AppService
         return _context.Providers
             .AsNoTracking()
             .ToList();
+    }
+
+    public async Task<IEnumerable<ServiceDisplay>> GetServicesAsync()
+    {
+        return await _context.Services
+            .Select(x => new ServiceDisplay
+            {
+                Id = x.Id,
+                Name = x.Name,
+            })
+            .ToListAsync();
+    }
+
+    public async Task CreateServiceAsync(CreateService createService)
+    {
+        await _context.Services.AddAsync(new Model.Entities.Service
+        {
+            Name = createService.Name
+        });
     }
 }
