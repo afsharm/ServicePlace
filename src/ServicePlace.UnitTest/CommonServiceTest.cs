c57cbf5445abfc9835c54b80539317c342f70c12
afshar <afshar.mohebbi@gmail.com>
2023-04-20 14:06:35 +0330

    using `xunit` Theory

--- src/ServicePlace.UnitTest/CommonServiceTest.cs
+++ src/ServicePlace.UnitTest/CommonServiceTest.cs
@@ -21,4 +21,22 @@ public class CommonServiceTest
         var exception = Record.Exception(() => commonService.ValidateCreateService(createService));
         Assert.Null(exception);
     }
+
+    [Theory]
+    [InlineData("String101abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz01234567890123")]
+    [InlineData("String104abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz01234567890123456")]
+    public void different_invalid_items_are_not_allowed(string value)
+    {
+        //Arrange
+        var commonService = new CommonService(null, null);
+
+        //Act
+        var createService = new CreateService
+        {
+            Name = value
+        };
+
+        //Assert
+        Assert.Throws<Exception>(() => commonService.ValidateCreateService(createService));
+    }
 }
