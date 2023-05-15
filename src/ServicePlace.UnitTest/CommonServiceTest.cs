846a4eac7543063098515c85041369b57d592c43
afshar <afshar.mohebbi@gmail.com>
2023-05-15 14:58:19 +0330

    async operation should be done in async methods

--- src/ServicePlace.UnitTest/CommonServiceTest.cs
+++ src/ServicePlace.UnitTest/CommonServiceTest.cs
@@ -12,7 +12,7 @@ public class CommonServiceTest : IClassFixture<TestDatabaseFixture>
     public TestDatabaseFixture Fixture { get; }
 
     [Fact]
-    public void Validate_create_should_not_throw_exception_for_ABC_string()
+    public async Task Validate_create_should_not_throw_exception_for_ABC_string()
     {
         //Arrange
         using var context = Fixture.CreateContext();
@@ -25,14 +25,14 @@ public class CommonServiceTest : IClassFixture<TestDatabaseFixture>
         };
 
         //Assert
-        var exception = Record.ExceptionAsync(() => commonService.CreateServiceAsync(createService));
+        var exception = await Record.ExceptionAsync(() => commonService.CreateServiceAsync(createService));
         Assert.Null(exception);
     }
 
     [Theory]
     [InlineData("String101abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz01234567890123")]
     [InlineData("String104abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz01234567890123456")]
-    public void different_invalid_items_are_not_allowed(string value)
+    public async Task different_invalid_items_are_not_allowed(string value)
     {
         //Arrange
         using var context = Fixture.CreateContext();
@@ -45,6 +45,6 @@ public class CommonServiceTest : IClassFixture<TestDatabaseFixture>
         };
 
         //Assert
-        Assert.ThrowsAsync<Exception>(() => commonService.CreateServiceAsync(createService));
+        await Assert.ThrowsAsync<Exception>(() => commonService.CreateServiceAsync(createService));
     }
 }
