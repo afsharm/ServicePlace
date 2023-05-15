16672cdcabe0af9030d77c069ac5b7df3c32328d
afshar <afshar.mohebbi@gmail.com>
2023-05-15 14:00:56 +0330

    a new test for checking addittions

--- src/ServicePlace.UnitTest/CommonServiceDbTest.cs
+++ src/ServicePlace.UnitTest/CommonServiceDbTest.cs
@@ -17,9 +17,27 @@ public class CommonServiceDbTest : IClassFixture<TestDatabaseFixture>
         var commonService = new CommonService(context, null);
 
         //Act
-        var list = await commonService.GetServicesAsync();;
+        var list = await commonService.GetServicesAsync(); ;
 
         //Assert
         Assert.Equal(2, list.Count());
+    }
+
+    [Fact]
+    public async Task Services_count_are_increased_after_adding_a_new_one()
+    {
+        //Arrange
+        using var context = Fixture.CreateContext();
+        var commonService = new CommonService(context, null);
+
+        //Act
+        var list_before = await commonService.GetServicesAsync();
+        await commonService.CreateServiceAsync(new CreateService());
+        await context.SaveChangesAsync();
+        var list_after = await commonService.GetServicesAsync();
+
+        //Assert
+        var diff = list_after.Count() - list_before.Count();
+        Assert.Equal(1, diff);
     }
 }
