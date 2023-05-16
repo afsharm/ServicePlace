babec365cd5ca4f5b72003382cc72493687a5ecb
afshar <afshar.mohebbi@gmail.com>
2023-05-16 10:45:36 +0330

    testing the service controller

---
+++ src/ServicePlace.UnitTest/ServiceControllerTest.cs
@@ -0,0 +1,30 @@
+using Microsoft.Extensions.Logging;
+using Moq;
+using ServicePlace.Service;
+using ServicePlace.Web.Controllers;
+
+[CollectionDefinition("TransactionalTests")]
+public class ServiceControllerTest : IClassFixture<TestDatabaseFixture>
+{
+    public ServiceControllerTest(TestDatabaseFixture fixture)
+        => Fixture = fixture;
+
+    public TestDatabaseFixture Fixture { get; }
+
+    [Fact]
+    public async Task GetServiceTest()
+    {
+        //Arrange
+        using var context = Fixture.CreateContext();
+        var loggerCommonService = Mock.Of<ILogger<CommonService>>();
+        var commonService = new CommonService(context, loggerCommonService);
+        var loggerServiceController = Mock.Of<ILogger<ServiceController>>();
+        var serviceController = new ServiceController(loggerServiceController, commonService, context);
+
+        //Act
+        var exception = await Record.ExceptionAsync(() => serviceController.GetServicesAsync());
+
+        //Assert
+        Assert.Null(exception);
+    }
+}
