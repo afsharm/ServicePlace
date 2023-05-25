23618c63fac7ee031ae4e51f9600664fb6de7d0f
afshar <afshar.mohebbi@gmail.com>
2023-05-25 11:31:56 +0330

    Mocking CreateServiceAsync

--- src/ServicePlace.UnitTest/ServiceControllerRepositoryTest.cs
+++ src/ServicePlace.UnitTest/ServiceControllerRepositoryTest.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.Logging;
 using Moq;
 using ServicePlace.Data.Contracts;
+using ServicePlace.Model.Commands;
 using ServicePlace.Service;
 using ServicePlace.Service.Contracts;
 using ServicePlace.Web.Controllers;
@@ -20,5 +21,21 @@ public class ServiceControllerRepositoryTest
         var loggerServiceController = Mock.Of<ILogger<ServiceController>>();
         var serviceController = new ServiceController(loggerServiceController, commonService);
         return serviceController;
+    }
+
+    [Fact]
+    public async Task create_service_works_on_simple_conditions()
+    {
+        //Arrange
+        var serviceController = BuildServiceController();
+        var serviceName = Guid.NewGuid().ToString();
+        var command = new CreateService { Name = serviceName };
+
+        //Action
+        var result = await serviceController.CreateServiceAsync(command);
+
+        //Assert
+        Assert.NotNull(result);
+        Assert.True(result.ServiceId > 0);
     }
 }
